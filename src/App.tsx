5374a4b903c66498bd1f07f022192d8f10111fce
NUWAHOORA <nuwahoorabrian02@gmail.com>
2026-02-20 11:12:32 +0300

    view

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { ViewModeProvider } from "@/contexts/ViewModeContext";
 
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -28,29 +29,31 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AuthProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/events" element={<EventsPage />} />
-            <Route path="/events/create" element={<CreateEventPage />} />
-            <Route path="/events/:id" element={<EventDetailPage />} />
-            <Route path="/analytics" element={<AnalyticsPage />} />
-            <Route path="/notifications" element={<NotificationsPage />} />
-            <Route path="/tickets" element={<TicketsPage />} />
-            <Route path="/approvals" element={<ApprovalsPage />} />
-            <Route path="/registrations" element={<RegistrationsPage />} />
-            <Route path="/attendance" element={<AttendancePage />} />
-            <Route path="/users" element={<UsersPage />} />
-            <Route path="/resources" element={<ResourcesPage />} />
-            <Route path="/meetings" element={<MeetingsPage />} />
-            <Route path="/email-settings" element={<EmailSettingsPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <ViewModeProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/events" element={<EventsPage />} />
+              <Route path="/events/create" element={<CreateEventPage />} />
+              <Route path="/events/:id" element={<EventDetailPage />} />
+              <Route path="/analytics" element={<AnalyticsPage />} />
+              <Route path="/notifications" element={<NotificationsPage />} />
+              <Route path="/tickets" element={<TicketsPage />} />
+              <Route path="/approvals" element={<ApprovalsPage />} />
+              <Route path="/registrations" element={<RegistrationsPage />} />
+              <Route path="/attendance" element={<AttendancePage />} />
+              <Route path="/users" element={<UsersPage />} />
+              <Route path="/resources" element={<ResourcesPage />} />
+              <Route path="/meetings" element={<MeetingsPage />} />
+              <Route path="/email-settings" element={<EmailSettingsPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ViewModeProvider>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
